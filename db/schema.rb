8c23845d2252c783d70c4023d3722bcd5ce7e113
David Bernheisel <david@bernheisel.com>
2015-10-15 17:51:44 -0400

    Update modal associations and destroy seeds on setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015190959) do
+ActiveRecord::Schema.define(version: 20151015213837) do
 
   create_table "categories", force: :cascade do |t|
     t.integer  "user_id"
