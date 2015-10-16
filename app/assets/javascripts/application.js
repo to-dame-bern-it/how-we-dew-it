f76a78aa2749f06b1b85426587e8cb5632099a65
David Bernheisel <david@bernheisel.com>
2015-10-16 14:12:44 -0400

    Update task index view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require html.sortable.min
 //= require_tree .
+
+
+$(function() {
+  $(".tasklist").sortable({
+
+  });
+
+  // $(".draggable").draggable({
+  //   connectToSortable: "#tasklist",
+  //   helper: "clone",
+  //   revert: "invalid"
+  // });
+
+  $(".draggable").disableSelection();
+
+  $('.sortable').sortable().bind('sortupdate', function(e, ui) {
+      /*
+
+      This event is triggered when the user stopped sorting and the DOM position has changed.
+
+      ui.item contains the current dragged element.
+      ui.index contains the new index of the dragged element (considering only list items)
+      ui.oldindex contains the old index of the dragged element (considering only list items)
+      ui.elementIndex contains the new index of the dragged element (considering all items within sortable)
+      ui.oldElementIndex contains the old index of the dragged element (considering all items within sortable)
+      ui.startparent contains the element that the dragged item comes from
+      ui.endparent contains the element that the dragged item was added to (new parent)
+
+      */
+  });
+});
+
