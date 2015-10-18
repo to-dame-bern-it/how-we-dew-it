db158602930792f7ece7e0f13744c8414150dfbf
David Bernheisel <david@bernheisel.com>
2015-10-18 16:52:17 -0400

    Fix sign up button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in?
+  before_action :require_login, only: [:index, :show, :edit, :update, :destroy]
+
 
   def index
     @users = User.all
